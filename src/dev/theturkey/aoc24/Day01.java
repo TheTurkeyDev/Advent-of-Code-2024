b943999041f2ea326bdfa1fab6bf1e7553c9f493
TheTurkeyDev <turkey@theturkey.dev>
2024-12-01 00:30:59 -0500

    Day 1 Code

--- src/dev/theturkey/aoc24/Day01.java
+++ src/dev/theturkey/aoc24/Day01.java
@@ -1,5 +1,6 @@
 package dev.theturkey.aoc24;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day01 extends AOCPuzzle
@@ -12,6 +13,30 @@ public class Day01 extends AOCPuzzle
 	@Override
 	void solve(List<String> input)
 	{
+		List<Integer> left = new ArrayList<>();
+		List<Integer> right = new ArrayList<>();
 
+		for(String in : input)
+		{
+			String[] parts = in.split(" {3}");
+			left.add(Integer.parseInt(parts[0]));
+			right.add(Integer.parseInt(parts[1]));
+		}
+
+		left.sort(Integer::compareTo);
+		right.sort(Integer::compareTo);
+
+		int answer = 0;
+		for(int i = left.size() - 1; i >= 0; i--)
+			answer += Math.abs(left.get(i) - right.get(i));
+		lap(answer);
+
+		answer = 0;
+		for(int i = left.size() - 1; i >= 0; i--)
+		{
+			int leftNum = left.get(i);
+			answer += (int) (leftNum * right.stream().filter(v -> v == leftNum).count());
+		}
+		lap(answer);
 	}
 }
