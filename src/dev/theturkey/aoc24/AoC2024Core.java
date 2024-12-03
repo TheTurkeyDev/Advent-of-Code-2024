33f75087ebc639fdcd95ecab5898ff0282b02dc4
TheTurkeyDev <turkey@theturkey.dev>
2024-12-03 00:20:15 -0500

    Day 3

--- src/dev/theturkey/aoc24/AoC2024Core.java
+++ src/dev/theturkey/aoc24/AoC2024Core.java
@@ -5,6 +5,7 @@ public class AoC2024Core
 	public static void main(String[] args)
 	{
 //		new Day01();
-		new Day02();
+//		new Day02();
+		new Day03();
 	}
 }
---
+++ src/dev/theturkey/aoc24/Day03.java
@@ -0,0 +1,52 @@
+package dev.theturkey.aoc24;
+
+import java.util.List;
+
+public class Day03 extends AOCPuzzle
+{
+	public Day03()
+	{
+		super("3");
+	}
+
+	@Override
+	void solve(List<String> input)
+	{
+		int sum = 0;
+		int sum2 = 0;
+		for(String l : input)
+		{
+			boolean ignore = false;
+			for(int i = 0; i < l.length(); i++)
+			{
+				if(l.charAt(i) == 'm' || l.charAt(i) == 'd')
+				{
+					int endIndex = l.indexOf(")", i);
+					String part = l.substring(i, endIndex + 1);
+					if(part.matches("mul\\(\\d+,\\d+\\)"))
+					{
+						String[] parts = part.substring(4, part.length() - 1).split(",");
+						int prod = Integer.parseInt(parts[0]) * Integer.parseInt(parts[1]);
+						sum += prod;
+						if(!ignore)
+							sum2 += prod;
+						i = endIndex;
+					}
+					else if(part.matches("do\\(\\)"))
+					{
+						ignore = false;
+						i = endIndex;
+					}
+					else if(part.matches("don't\\(\\)"))
+					{
+						ignore = true;
+						i = endIndex;
+					}
+				}
+			}
+		}
+
+		lap(sum);
+		lap(sum2);
+	}
+}
