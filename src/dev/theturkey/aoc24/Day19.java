87b98e458987bcb6ba759fa6f9a7c3ba0ed7a18e
TheTurkeyDev <turkey@theturkey.dev>
2024-12-19 09:51:09 -0500

    Day 19 Part 2

--- src/dev/theturkey/aoc24/Day19.java
+++ src/dev/theturkey/aoc24/Day19.java
@@ -1,6 +1,5 @@
 package dev.theturkey.aoc24;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -8,48 +7,40 @@ import java.util.Map;
 
 public class Day19 extends AOCPuzzle
 {
+	private static final Map<String, Long> cache = new HashMap<>();
+
 	public Day19()
 	{
 		super("19");
 	}
 
-	private List<String> getTowelComposition(Map<Integer, List<String>> routes, int index, List<String> towelsUsed)
+	private long getPatternCompositions(String pattern, List<String> towels)
 	{
-		if(index == routes.size() - 1)
-			return new ArrayList<>();
+		if(cache.containsKey(pattern))
+			return cache.get(pattern);
 
-		for(String route : routes.getOrDefault(index, new ArrayList<>()))
-		{
-			List<String> newPath = new ArrayList<>(towelsUsed);
-			newPath.add(route);
-			List<String> possiblePath = getTowelComposition(routes, index + route.length(), newPath);
-			if(possiblePath != null)
-				return possiblePath;
-		}
-		return null;
+		long ret = 0;
+		for(String t : towels)
+			if(pattern.startsWith(t))
+				ret += getPatternCompositions(pattern.substring(t.length()), towels);
+		cache.put(pattern, ret);
+		return ret;
 	}
 
 	@Override
 	public void solve(List<String> input)
 	{
 		List<String> towels = Arrays.stream(input.get(0).split(", ")).toList();
-
+		cache.put("", 1L);
 		int part1 = 0;
+		long part2 = 0;
 		for(String pattern : input.subList(2, input.size()))
 		{
-			Map<Integer, List<String>> routes = new HashMap<>();
-			for(int i = 0; i < pattern.length(); i++)
-			{
-				String patternSub = pattern.substring(i);
-				for(String towel : towels)
-					if(patternSub.startsWith(towel))
-						routes.computeIfAbsent(i, k -> new ArrayList<>()).add(towel);
-			}
-
-			List<String> path = getTowelComposition(routes, 0, new ArrayList<>());
-			if(path != null)
-				part1++;
+			long paths = getPatternCompositions(pattern, towels);
+			part1 += paths > 0 ? 1 : 0;
+			part2 += paths;
 		}
 		lap(part1);
+		lap(part2);
 	}
 }
